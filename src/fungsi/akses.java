d820502855374df87aa0c4e45cdee15b5e31296b
nfebriand <nanda.febriand@gmail.com>
2026-01-20 11:23:27 +0700

    wip

--- src/fungsi/akses.java
+++ src/fungsi/akses.java
@@ -240,7 +240,7 @@ public final class akses {
             skrining_kanker_kolorektal=false,dapur_pemesanan=false,bayar_pesan_dapur=false,hutang_dapur=false,titip_faktur_dapur=false,validasi_tagihan_dapur=false,
             surat_pemesanan_dapur=false,pengajuan_barang_dapur=false,dapur_returbeli=false,hibah_dapur=false,ringkasan_penerimaan_dapur=false,ringkasan_pengajuan_dapur=false,
             ringkasan_pemesanan_dapur=false,ringkasan_returbeli_dapur=false,ringkasan_stokkeluar_dapur=false,dapur_stokkeluar_pertanggal=false,sirkulasi_dapur=false,
-            sirkulasi_dapur2=false,verifikasi_penerimaan_dapur=false,nilai_penerimaan_vendor_dapur_perbulan=false,ringkasan_hutang_vendor_dapur=false,pindah_kamar_pilihan_2=false,apt_restore=false,penilaian_psikologi_klinis=false,
+            sirkulasi_dapur2=false,verifikasi_penerimaan_dapur=false,nilai_penerimaan_vendor_dapur_perbulan=false,ringkasan_hutang_vendor_dapur=false,penilaian_psikologi_klinis=false,
             penilaian_awal_medis_ranap_neonatus=false,penilaian_derajat_dehidrasi=false,ringkasan_jasa_tindakan_medis=false,pendapatan_per_akun=false,hasil_pemeriksaan_echo=false,
             penilaian_bayi_baru_lahir=false,rl1_3_ketersediaan_kamar=false,pendapatan_per_akun_closing=false,pengeluaran_pengeluaran=false,skrining_diabetes_melitus=false,
             laporan_tindakan=false,pelaksanaan_informasi_edukasi=false,layanan_kedokteran_fisik_rehabilitasi=false,skrining_kesehatan_gigi_mulut_balita=false,skrining_anemia=false,
@@ -270,7 +270,8 @@ public final class akses {
         bpjs_riwayat_pelayanan_obat_smc = false,
         bpjs_riwayat_pelayanan_resep_smc = false,
         set_pintu_poli = false,
-        pintu_poli = false;
+        pintu_poli = false,
+        apt_restore=false;
 
     public static void setData(String user, String pass){
         int retries=2;
--- src/fungsi/koneksiDB.java
+++ src/fungsi/koneksiDB.java
@@ -52,7 +52,7 @@ public class koneksiDB {
                                     "  #    ___) || | | |  | ||  _ <  ___) | | . \\ | | | || (_| || | | | / /| (_| |\n" +
                                     "  #   |____/|___||_|  |_||_| \\_\\|____/  |_|\\_\\|_| |_| \\__,_||_| |_|/___|\\__,_|\n" +
                                     "  #                                                                           \n\n"+
-                                    "  Versi : c1733dad5f47a0e739e6cdcc9d66919441bac966 15/12/2025 \n\n"+
+                                    "  Versi : 29aa58288704d7c9e4a031926e85a87edbbce50d Januari/2026 \n\n"+
                                     "  Lisensi yang dianut di software ini https://en.wikipedia.org/wiki/Aladdin_Free_Public_License \n"+                                    
                                     "  Informasi dan panduan bisa dicek di halaman https://github.com/mas-elkhanza/SIMRS-Khanza/wiki \n"+                                    
                                     "  Bagi yang ingin berdonasi untuk pengembangan aplikasi ini bisa ke BSI 1015369872 atas nama Windiarto");
