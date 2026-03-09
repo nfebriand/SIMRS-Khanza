7f3fd6d062e2dccbcdab21b82657b4a3395c474f
nfebriand <nanda.febriand@gmail.com>
2026-03-09 09:53:19 +0700

    fix

--- src/fungsi/koneksiDB.java
+++ src/fungsi/koneksiDB.java
@@ -74,7 +74,7 @@ public class koneksiDB {
         dataSource.setURL("jdbc:mysql://"+EnkripsiAES.decrypt(prop.getProperty("HOST"))+":"+EnkripsiAES.decrypt(prop.getProperty("PORT"))+"/"+EnkripsiAES.decrypt(prop.getProperty("DATABASE"))+"?zeroDateTimeBehavior=convertToNull&tcpKeepAlive=true&connectTimeout=10000&socketTimeout=60000&maintainTimeStats=false");
         dataSource.setUser(EnkripsiAES.decrypt(prop.getProperty("USER")));
         dataSource.setPassword(EnkripsiAES.decrypt(prop.getProperty("PAS")));
-        dataSource.setCachePreparedStatements(true);
+        // dataSource.setCachePreparedStatements(true);
         dataSource.setUseCompression(true);
     }
 
@@ -795,6 +795,16 @@ public class koneksiDB {
         try (FileInputStream fis = new FileInputStream("setting/database.xml")) {
             prop.loadFromXML(fis);
             var=EnkripsiAES.decrypt(prop.getProperty("USERKEYAPIAPOTEKBPJS"));
+        }catch(Exception e){
+            var="";
+        }
+        return var;
+    }
+
+    public static String KODEPPKAPOTEKBPJS(){
+        try (FileInputStream fis = new FileInputStream("setting/database.xml")) {
+            prop.loadFromXML(fis);
+            var=EnkripsiAES.decrypt(prop.getProperty("KODEPPKAPOTEKBPJS"));
         }catch(Exception e){
             var="";
         }
